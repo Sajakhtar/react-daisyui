8729470b27fc53d4c38add4315b5a317e5734f60
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2022-02-09 23:20:56 +0400

    move hero to it's own component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
-
-      <div className="hero min-h-screen bg-base-200">
-        <div className="text-center hero-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">
-                  Hello there
-                </h1>
-            <p className="mb-5">
-                  Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
-                </p>
-            <button className="btn btn-primary">Connect Wallet</button>
-          </div>
-        </div>
-      </div>
-
+      <Hero />
     </div>
   );
 }
