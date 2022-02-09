a7bc25082968e8d36aa5716c5788012a4607693e
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2022-02-09 23:03:48 +0400

    update app.js - replace class with className

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ function App() {
   return (
     <div className="App">
 
-      <div class="hero min-h-screen bg-base-200">
-        <div class="text-center hero-content">
-          <div class="max-w-md">
-            <h1 class="mb-5 text-5xl font-bold">
+      <div className="hero min-h-screen bg-base-200">
+        <div className="text-center hero-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">
                   Hello there
                 </h1>
-            <p class="mb-5">
+            <p className="mb-5">
                   Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
                 </p>
-            <button class="btn btn-primary">Connect Wallet</button>
+            <button className="btn btn-primary">Connect Wallet</button>
           </div>
         </div>
       </div>
