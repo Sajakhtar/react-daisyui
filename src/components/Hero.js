02e029428818461cc87a1b2836c7eff9e5e23ad6
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2022-02-10 16:17:11 +0400

    install tailwind heroicons pkg, add login icon in the button of the hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,3 +1,4 @@
+import { LoginIcon } from '@heroicons/react/outline';
 
 function Hero() {
   return (
@@ -10,7 +11,10 @@ function Hero() {
           <p className="mb-5">
                 Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
               </p>
-          <button className="btn btn-primary">Connect Wallet</button>
+          <button className="btn btn-primary">
+            <LoginIcon className="h-5 w-5 mr-2"/>
+            Connect Wallet
+          </button>
         </div>
       </div>
       </div>
